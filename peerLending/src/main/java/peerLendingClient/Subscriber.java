f0d1944d558b6b593d263120c4dac694b2a0c0e4
Ana Rodrigues <a78763@alunos.uminho.pt>
2019-01-11 22:52:56 +0000

    Inserido Subscriber (Cliente feito.)

---
+++ peerLending/src/main/java/peerLendingClient/Subscriber.java
@@ -0,0 +1,43 @@
+package peerLendingClient;
+
+import org.zeromq.ZMQ;
+
+
+public class Subscriber implements Runnable {
+    ZMQ.Context context;
+    ZMQ.Socket subscriber;
+
+
+    public Subscriber (ZMQ.Context context, ZMQ.Socket subscriber) {
+        this.context = context;
+        this.subscriber = subscriber;
+    }
+
+    public void run() {
+        this.subscriber.connect("tcp://localhost:8888");
+
+        try {
+            while (true) {
+                System.out.println("########### NEW NOTIFICATION ##########");
+                String notification = this.subscriber.recvStr();
+                System.out.println(notification);
+                /*String[] notification = this.subscriber.recvStr().split(":");
+                System.out.println(notification[0]);
+                System.out.println("> ID: " + notification[1]);
+                System.out.println("> Amount: " + notification[2]);
+                System.out.println("> Interest: " + notification[3]);
+                if (notification[0].equals("auction"))
+                    System.out.println("> Bids: " + notification[4]);
+                else
+                    System.out.println("> Subscriptions: " + notification[4]);*/
+            }
+        }
+        catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
+        finally {
+            this.subscriber.close();
+            this.context.term();
+        }
+    }
+}
